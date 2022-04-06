5072a6a54cfdaec6193a4a0dd0a44b978f2f8cc6
FrankLeFebvre2023 <70653559+FrankLeFebvre2023@users.noreply.github.com>
2022-04-06 11:04:09 -0400

    Created login activity, and made it the default activity

---
+++ app/src/main/java/com/example/eventir/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.eventir;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
+}
