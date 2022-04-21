63c8c735b53cc9ea541ee59556c43f40394777de
NJSal <salazar.davidf@gmail.com>
2022-04-20 22:46:42 -0400

    commit: continuing work with EventsPlannedAdapter

--- app/src/main/java/com/example/eventir/adapters/EventsPlannedAdapter.java
+++ app/src/main/java/com/example/eventir/adapters/EventsPlannedAdapter.java
@@ -1,4 +1,28 @@
 package com.example.eventir.adapters;
 
-public class EventsPlannedAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EventsPlannedAdapter extends RecyclerView.Adapter<EventsPlannedAdapter.ViewHolder>{
+    
+    @NonNull
+    @Override
+    public EventsPlannedAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventsPlannedAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder {
+    }
 }
---
+++ app/src/main/java/com/example/eventir/models/EventsPlanned.java
@@ -0,0 +1,17 @@
+package com.example.eventir.models;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Event")
+public class EventsPlanned extends ParseObject {
+
+}
