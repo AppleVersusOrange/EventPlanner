f0cd2ac97361eb325f765978747f6de75de21fd8
Jacob White <jjw37@njit.edu>
2022-05-02 20:33:26 -0400

    added in dropdown menu and filtering event search by ID.

--- app/src/main/java/com/example/eventir/networking/TicketMasterClient.java
+++ app/src/main/java/com/example/eventir/networking/TicketMasterClient.java
@@ -16,9 +16,26 @@ public class TicketMasterClient  {
         this.client = new AsyncHttpClient();
     }
     //change to also take in second argument the zipcode.
-    public void getEvents(JsonHttpResponseHandler handler, String latLong) {
+    public void getEvents(JsonHttpResponseHandler handler, String latLong, String genre) {
+        String genreID;
         String apiUrl = BASE_URL + "events.json";
         RequestParams params = new RequestParams();
+        if(genre.equals("Sports")) {
+            genreID = "KZFzniwnSyZfZ7v7nE";
+            params.put("classificationId",genreID);
+        }
+        else if(genre.equals("Music")) {
+            genreID = "KZFzniwnSyZfZ7v7nJ";
+            params.put("classificationId",genreID);
+        }
+        else if(genre.equals("Arts")) {
+            genreID = "KZFzniwnSyZfZ7v7na";
+            params.put("classificationId",genreID);
+        }
+        else if(genre.equals("Film")) {
+            genreID = "KZFzniwnSyZfZ7v7nn";
+            params.put("classificationId",genreID);
+        }
         //change this to get the user's zipcode by from DB.
         params.put("apikey", REST_CONSUMER_KEY);
         params.put("latlong", latLong);
